3eb6579930e2a6e3a51c9eef7ae82a3784a23501
Robot <1750041@qq.com>
2021-10-11 17:47:38 +0800

    fix path

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .target(
             name: "MulitPing",
             dependencies: [],
-            path: "Source"
+            path: "Sources"
         )
     ]
 )
