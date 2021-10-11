ff4e87f7520ce044c08f18fe665900820ba4928c
Robot <1750041@qq.com>
2021-10-11 18:01:51 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -1,14 +1,9 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "MulitPing",
-    platforms: [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .tvOS(.v9)
-    ],
     products: [
         .library(
           name: "MulitPing",
@@ -18,8 +13,7 @@ let package = Package(
     targets: [
         .target(
             name: "MulitPing",
-            dependencies: [],
-            path: "Sources"
+            dependencies: []
         )
     ]
 )
